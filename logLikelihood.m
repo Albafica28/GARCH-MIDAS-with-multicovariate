9151db91a6e341979c2fb9b96d68a133d53431a9
Wenhui Shi <albafica28@163.com>
2024-01-29 18:31:30 +0100

    checking robustness

--- logLikelihood.m
+++ logLikelihood.m
@@ -1,4 +1,4 @@
-function logL = logLikelihood(params, X, Y, K)
+function logL = logLikelihood(params, X, Y, K, isGJR)
     %nHighFreq = size(X, 1);
     nPeriods = 22;
     nSamples = length(X);
@@ -11,6 +11,8 @@ function logL = logLikelihood(params, X, Y, K)
     mu    = params(1);
     alpha = params(2);
     beta  = params(3);
+    if isGJR
+
     m0    = params(4);
     w1    = params(5:4+nV);
     theta = params(5+nV:4+2*nV);
@@ -31,22 +33,19 @@ function logL = logLikelihood(params, X, Y, K)
     % Conditional variance recursion
     % Fixed tau in a week/month/quarter/year
     % Compute MIDAS weights
-    weights = midasBetaWeights(K, w1, []);
+    weights = BetaFun(K, w1, []);
     % Compute GARCH-MIDAS long-run and short-run variance components
     % The first nlag columns are unassigned due to missing presample values
     for t = K+1:nLowFreq
-        % Compute long-run component
-        % Refer to Eq (5) in Engle et al. (2013)
+        % Compute long-run component with multivariable
         tau = exp(m0 + theta' * diag(weights * Y(t-K:t-1, :)) );
         alphaTau = alpha ./ tau;
         % Compute short-run component
-        % Refer to Eq (4) in Engle et al. (2013)
         for n = 1:nPeriods
             ind = (t-1)*nPeriods + n;
             ShortRun(ind) = omega + alphaTau .* epsilon2(ind-1) + beta .* ShortRun(ind-1);
         end
         % Compute conditional variance
-        % Refer to Eq (3) in Engle et al. (2013)
         Variance(:, t) = tau .* ShortRun(:, t);
     end
     
@@ -55,15 +54,12 @@ function logL = logLikelihood(params, X, Y, K)
     logLMatrix(isinf(logLMatrix)) = 1e6;
     logLMatrix(:, 1) = 0;
     logL = sum(logLMatrix(:));
-    if isnan(logL)
-        a = 1;
-    end
 end
 
 
-function weights = midasBetaWeights(nlag, param1, param2)
+function weights = BetaFun(nlag, param1, param2)
     seq = nlag:-1:1; 
-    if isempty(param2)    
+    if isempty(param2)
         weights = (1-seq./nlag+10*eps).^(param1-1);    
     else
         weights = (1-seq./nlag+10*eps).^(param1-1) .* (seq./nlag).^(param2-1);    
