d69f7734073aca0961ecbb95a599539fc2a6734e
Wenhui Shi <albafica28@163.com>
2024-02-12 09:58:56 +0100

    debug

--- demo.m
+++ demo.m
@@ -2,11 +2,11 @@ clc
 clear
 close all
 rng default
-load dataset_market1.mat
+load dataset.mat
 
 K = 10;
 XDate = HighFreqDate;
 YDate = LowsFreqDate;
 [estMdl, sigmat, zt, Xt, Xidx, XDateMat] = ...
-        modelFit(X(:, 1), Y, 'nLags', K, 'XDate', XDate, 'YDate', YDate);
+        GARCHmidasFit(X(:, 1), Y, 'nLags', K, 'XDate', XDate, 'YDate', YDate);
 disp(estMdl.resultTab)
